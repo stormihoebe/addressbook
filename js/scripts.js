a7065a4ed973d5f1cc024a50e0b1d213f91cef04
Evan Bishop and Stormi Hoebelheinrich <evan@>
2017-02-28 09:17:36 -0800

    add second home and work address and display all addresses inputted

--- js/scripts.js
+++ js/scripts.js
@@ -19,38 +19,72 @@ Address.prototype.fullAddress = function() {
 $(document).ready(function() {
 //add new address form
   $("#add-address").click(function() {
-    $("#new-addresses").append('<div class="new-address">' +
+    $("#new-addresses").append('<div class="new-second-address">' +
                                  '<div class="form-group extra">' +
+                                 '<h2>Second Home</h2>' +
                                    '<label for="new-street">Street</label>' +
-                                   '<input type="text" class="form-control new-street">' +
+                                   '<input type="text" class="form-control new-street" id="second-street">' +
                                  '</div>' +
                                  '<div class="form-group extra">' +
                                    '<label for="new-city">City</label>' +
-                                   '<input type="text" class="form-control new-city">' +
+                                   '<input type="text" class="form-control new-city"id="second-city">' +
                                  '</div>' +
                                  '<div class="form-group extra">' +
                                    '<label for="new-state">State</label>' +
-                                   '<input type="text" class="form-control new-state">' +
+                                   '<input type="text" class="form-control new-state"id="second-state">' +
                                  '</div>' +
-                               '</div>');
+                               '</div>' +
+                               '<div class="new-work-address">' +
+                                  '<div class="form-group extra">' +
+                                  '<h2>Work Address</h2>' +
+                                    '<label for="new-street">Street</label>' +
+                                    '<input type="text" class="form-control new-street" id="work-street">' +
+                                  '</div>' +
+                                  '<div class="form-group extra">' +
+                                    '<label for="new-city">City</label>' +
+                                    '<input type="text" class="form-control new-city" id="work-city">' +
+                                  '</div>' +
+                                  '<div class="form-group extra">' +
+                                    '<label for="new-state">State</label>' +
+                                    '<input type="text" class="form-control new-state" id="work-state">' +
+                                  '</div>' +
+                                '</div>'
+                            );
   });
   //add contact submit
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
-    $(".extra").remove();
+    // debugger;
 
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
     var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
     $(".new-address").each(function() {
+//First address inputs
       var inputtedStreet = $(this).find("input#new-street").val();
       var inputtedCity = $(this).find("input#new-city").val();
       var inputtedState = $(this).find("input#new-state").val();
-      var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState)
+      var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState);
       newContact.addresses.push(newAddress)
     });
-
+// second inputs
+    $(".new-second-address").each(function() {
+        inputtedSecondStreet = $(this).find("input#second-street").val();
+        inputtedSecondCity = $(this).find("input#second-city").val();
+        inputtedSecondState = $(this).find("input#second-state").val();
+        newSecondAddress = new Address(inputtedSecondStreet, inputtedSecondCity, inputtedSecondState)
+      newContact.addresses.push(newSecondAddress);
+      });
+// work address inputs
+    $(".new-work-address").each(function() {
+        inputtedWorkStreet = $(this).find("input#work-street").val();
+        inputtedWorkCity = $(this).find("input#work-city").val();
+        inputtedWorkState = $(this).find("input#work-state").val();
+        newWorkAddress = new Address(inputtedWorkStreet, inputtedWorkCity, inputtedWorkState)
+      newContact.addresses.push(newWorkAddress);
+    $(".extra").remove();
+  });
     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
     $(".contact").last().click(function() {
